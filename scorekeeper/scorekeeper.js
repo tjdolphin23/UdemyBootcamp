b9512a5316fca0a7c27422a83eab8461cf7f5aa7
Tom Dolphin <thedolphingroup1@gmail.com>
2018-06-09 08:36:51 -0700

    completed reset button and create css style to winning score

--- scorekeeper/scorekeeper.js
+++ scorekeeper/scorekeeper.js
@@ -1,5 +1,6 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
+var reset = document.querySelector("#reset");
 var p1Display = document.querySelector("#p1Display");
 var p2Display= document.querySelector("#p2Display");
 var p1Score = 0;
@@ -13,6 +14,7 @@ p1Button.addEventListener("click", function() {
 	if(!gameOver) {
 		p1Score++;
 			if(p1Score === winningScore){
+				p1Display.classList.add("winner");
 				gameOver = true;
 			}
 	p1Display.textContent = p1Score;
@@ -23,9 +25,20 @@ p2Button.addEventListener("click", function() {
 	if(!gameOver) {
 		p2Score++;
 			if(p2Score === winningScore){
+				p2Display.classList.add("winner");
 				gameOver = true;
 			}
 	p2Display.textContent = p2Score;
 }
 }); 
+
+reset.addEventListener("click", function() {
+	p1Score = 0;
+	p2Score = 0;
+	p1Display.textContent = 0;
+	p2Display.textContent =0;
+	p1Display.classList.remove("winner");
+	p2Display.classList.remove("winner");
+	gameOver = false;
+});
 
