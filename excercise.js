2492f451f476b8e8bad08676e7486d2c6c765eef
Tom Dolphin <thedolphingroup1@gmail.com>
2018-06-20 13:24:25 -0700

    final draft completed

---
+++ excercise.js
@@ -0,0 +1,12 @@
+	// //Select all divs & give purple background
+	// $("div").css("background", "purple");
+
+	// //select all divs with class highlighted and make them 200px wide
+	// $(".highlight").css("width", "200px");
+
+	// //Select div w/ id third & give orange border
+	// $("#third").css("border", "orange");
+
+	// //bonus select first div only & change font color pink
+	// $("div:first").css("color", "pink");
+	alert("connected")
---
+++ exercise.js
@@ -0,0 +1,11 @@
+	//Select all divs & give purple background
+	$("div").css("background", "purple");
+
+	//select all divs with class highlighted and make them 200px wide
+	$(".highlight").css("width", "200px");
+
+	//Select div w/ id third & give orange border
+	$("#third").css("border", "4px solid orange");
+
+	//bonus select first div only & change font color pink
+	$("div:first").css("color", "pink");
