ed99f9a191b03bc38651c570d9fc1c87c96d9d9a
Tom Dolphin <thedolphingroup1@gmail.com>
2018-09-13 09:39:35 -0700

    Created blog app, practicing rest routes, set up initial config & index app

---
+++ BlogApp/app.js
@@ -0,0 +1,56 @@
+var express = require("express");
+app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+
+//APP Config
+mongoose.connect("mongodb://localhost/blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Mongoose/Model Config
+var blogSchema = new mongoose.Schema({
+	title: String,
+	image: String,
+	body: String,
+	created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+
+//RESTFUL ROUTES
+app.get("/", function(req, res){
+	res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res){
+	Blog.find({}, function(err, blogs){
+		if(err) {
+			console.log(err);
+		} else {
+			res.render("index", {blogs: blogs});
+		}
+	})
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var PORT = process.env.PORT || 8080;
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
+
