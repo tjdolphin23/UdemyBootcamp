6f8d231acd6384ab59ea5578627363970967040b
Tom Dolphin <thedolphingroup1@gmail.com>
2018-09-14 09:52:58 -0700

    created edit page, inital edit route on app.js

--- BlogApp/app.js
+++ BlogApp/app.js
@@ -66,7 +66,16 @@ app.get("/blogs/:id", function(req, res){
 });
 
 
-
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err) {
+			res.redirect("/blogs");
+		} else {
+			res.render("edit", {blog: foundBlog});
+		}
+	})
+});
 
 
 
