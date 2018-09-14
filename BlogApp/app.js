62af664092804d668bbfd3af557662a045f78331
Tom Dolphin <thedolphingroup1@gmail.com>
2018-09-14 08:39:32 -0700

    created post route & basic design of form

--- BlogApp/app.js
+++ BlogApp/app.js
@@ -43,6 +43,17 @@ app.get("/blogs/new", function(req, res){
 });
 
 //CREATE ROUTE
+app.post("/blogs", function(req, res){
+	//creae blog
+	Blog.create(req.body.blog, function(err, newBlog){
+		if(err) {
+			res.render("new");
+		} else {
+			res.redirect("/blogs");
+		}
+	})
+	//redirect to blog
+});
 
 
 
