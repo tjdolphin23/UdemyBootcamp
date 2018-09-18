da1ed97771f4f65f6d22963ebdb8d0d332ff4cf1
Tom Dolphin <thedolphingroup1@gmail.com>
2018-09-18 08:55:06 -0700

    install express sanitizer on creae and update routes

--- BlogApp/app.js
+++ BlogApp/app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var methodOverride = require("method-override");
+var expressSanitizer = require("express-sanitzer");
 app = express();
 
 
@@ -10,7 +11,9 @@ mongoose.connect("mongodb://localhost/blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.use(methodOverride("_method"));
+
 
 //Mongoose/Model Config
 var blogSchema = new mongoose.Schema({
@@ -48,6 +51,7 @@ app.get("/blogs/new", function(req, res){
 //CREATE ROUTE
 app.post("/blogs", function(req, res){
 	//creae blog
+	req.body.blog.body = req.sanitize(req.body.blog.body);
 	Blog.create(req.body.blog, function(err, newBlog){
 		if(err) {
 			res.render("new");
@@ -84,6 +88,7 @@ app.get("/blogs/:id/edit", function(req, res){
 
 // UPDATE ROUTE
 app.put("/blogs/:id", function(req, res){
+	req.body.blog.body = req.sanitize(req.body.blog.body);
 	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
 		if(err) {
 			res.redirect("/blogs");
