dffe256dc4aeab1e8ebce02e3fb350dfad17ee8f
Tom Dolphin <thedolphingroup1@gmail.com>
2018-09-21 09:10:22 -0700

    created user.js find wiht user schema, required packages

--- authentication/authDemo/app.js
+++ authentication/authDemo/app.js
@@ -1,5 +1,11 @@
-var express = require("express");
-var mongoose = require("mongoose");
+var express 				= require("express"),
+	mongoose 				= require("mongoose"),
+	passport				= require("passport"),
+	bodyParser 				= require("body-parser"),
+	User 					= require("./models/user"),
+	LocalStrategy			= require("passport-local"),
+	passportLocalMongoose	= require("passport-local-mongoose")
+
 var app = express();
 
 app.set("view engine", "ejs");
